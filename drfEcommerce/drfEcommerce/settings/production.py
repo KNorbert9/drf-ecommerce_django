22717f36c8256bd4e8b2fa1728deecc5d2f3de60
Norbert KOHOKA <nkohoka@gmail.com>
2024-04-11 13:54:49 +0000

    first one

---
+++ drfEcommerce/drfEcommerce/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+ALLOWED_HOSTS = ["*"]
